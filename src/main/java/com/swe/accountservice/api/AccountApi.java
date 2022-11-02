ca2e97be7d7f821381ef7bf1b0ee7349b10df4c2
serbalta <salih.773@gmail.com>
2022-11-02 19:51:09 +0100

    addedFiles

---
+++ src/main/java/com/swe/accountservice/api/AccountApi.java
@@ -0,0 +1,39 @@
+package com.swe.accountservice.api;
+
+import com.swe.accountservice.entity.Account;
+import com.swe.accountservice.service.AccountService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("account")
+/**
+ * localhost:8080/account
+ */
+public class AccountApi {
+
+    private final AccountService accountService;
+
+    public AccountApi(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Account> get(@PathVariable("id") String id){
+       return ResponseEntity.ok(accountService.get(id));
+    }
+    @PostMapping
+    public ResponseEntity<Account> save(@RequestBody Account account){
+        return ResponseEntity.ok(accountService.save(account));
+    }
+    @PutMapping
+    public ResponseEntity<Account> update(Account account){
+        return ResponseEntity.ok(accountService.update(account));
+    }
+    @DeleteMapping
+    public void delete(String id){
+        accountService.delete(id);
+    }
+
+
+}
---
+++ src/main/java/com/swe/accountservice/entity/Account.java
@@ -0,0 +1,54 @@
+package com.swe.accountservice.entity;
+
+public class Account {
+    private String id;
+    private String userName;
+    private String eMail;
+    private String passwrd;
+
+    public Account(String id, String userName, String eMail, String passwrd) {
+        this.id = id;
+        this.userName = userName;
+        this.eMail = eMail;
+        this.passwrd = passwrd;
+    }
+
+    public Account() {
+    }
+
+    public Account(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getPasswrd() {
+        return passwrd;
+    }
+
+    public void setPasswrd(String passwrd) {
+        this.passwrd = passwrd;
+    }
+}
