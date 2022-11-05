c654fe388d5ab0adebd234fe3802bbac1bdb4001
serbalta <salih.773@gmail.com>
2022-11-05 20:51:35 +0100

    gittest

---
+++ src/main/java/com/swe/accountservice/service/AccountService.java
@@ -0,0 +1,25 @@
+package com.swe.accountservice.service;
+
+import com.swe.accountservice.entity.Account;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountService {
+
+    public Account get(String id){
+        return new Account("test-id");
+    }
+
+    public Account save(Account account){
+        return new Account("test-id");
+    }
+
+    public Account update(Account account){
+        return new Account("test-id");
+    }
+
+    public void delete(String id){
+
+    }
+
+}
